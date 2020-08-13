a58cf9056e68780cb64764c80a190e5f39573d20
SuperSus <nickvasilevski35@gmail.com>
2020-08-14 01:05:39 +0300

    add setting model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_143027) do
+ActiveRecord::Schema.define(version: 2020_08_13_214939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,16 @@ ActiveRecord::Schema.define(version: 2020_08_13_143027) do
     t.datetime "updated_at"
     t.string "cron"
     t.index ["priority", "run_at"], name: "delayed_jobs_priority"
+  end
+
+  create_table "settings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.jsonb "filters"
+    t.integer "last_sended_tender_id"
+    t.string "keywords"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_settings_on_user_id"
   end
 
   create_table "tenders", force: :cascade do |t|
