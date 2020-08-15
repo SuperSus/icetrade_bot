d9d8f951e10b81ac3a6bdddb6a0a2a057d835a4c
SuperSus <nickvasilevski35@gmail.com>
2020-08-15 17:22:44 +0300

    migrate keywords from string to string[]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_214939) do
+ActiveRecord::Schema.define(version: 2020_08_15_135541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2020_08_13_214939) do
     t.bigint "user_id"
     t.jsonb "filters"
     t.integer "last_sended_tender_id"
-    t.string "keywords"
+    t.string "keywords", default: [], array: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_settings_on_user_id"
