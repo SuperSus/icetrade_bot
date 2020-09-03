cf9213e6f9c2ae03d0bf397c8b2dbcec07343e2d
SuperSus <sikorskyalexandr08@gmail.com>
2020-09-03 15:45:58 +0300

    add default free tier subscription when user created

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,17 @@
 
 class User < ApplicationRecord
   has_one :setting
+  has_one :subscription, dependent: :destroy
 
-  before_create :initialize_setting
+  before_create :initialize_setting, :initialize_subscription
 
   private
 
   def initialize_setting
     build_setting
+  end
+
+  def initialize_subscription
+    self.subscription = Subscription.build_with_free_tier(id)
   end
 end
