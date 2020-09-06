91adeb393001a08c07967dad784d274f9483cddc
SuperSus <sikorskyalexandr08@gmail.com>
2020-09-07 00:46:06 +0300

    add separate payments controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,17 +1,7 @@
-class PaymentsController < ApplicationController
-  # POST /payments
-  def create
-    # if unaccepted - true send to user info
-    if PaymentProcessor.new(params: payment_params).run
-      head :ok
-    else
-      head :unprocessable_entity
-    end
-  end
-
-  private
-
-  def payment_params
-    params.permit(:amount, :sender, :label, :bill_id, :sha1_hash, :operation_id, :email, :phone, :city, :datetime, :codepro, :unaccepted, :test_notification)
+class PaymentsController <  ActionController::Base
+  append_view_path Rails.root.join("/app/views")
+  layout 'application'
+  
+  def new
   end
 end
